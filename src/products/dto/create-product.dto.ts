51bf4b38661f5fd636ce29ec17908f48f4a9fc6a
Igor BALLO <balloigor66@gmail.com>
2023-10-09 19:38:54 +0000

    add validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,21 @@
+import {IsNotEmpty, IsNumber, IsPositive, IsString, Min} from "class-validator";
+
 export class CreateProductDto {
+    @IsNotEmpty()
+    @IsString()
     name: string
+
+    @IsNotEmpty()
+    @IsString()
     description: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    @Min(1)
     price: number
+
+    @IsNotEmpty()
+    @IsString()
     supplierId: string
 }
--- src/suppliers/dto/create-supplier.dto.ts
+++ src/suppliers/dto/create-supplier.dto.ts
@@ -1,4 +1,11 @@
+import {IsNotEmpty, IsString} from "class-validator";
+
 export class CreateSupplierDto {
+    @IsString()
+    @IsNotEmpty()
     name: string
+
+    @IsString()
+    @IsNotEmpty()
     location: string
 }
