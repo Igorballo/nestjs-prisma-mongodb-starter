1c6d61d27d03845d71fd9f8ade4dd1c46ec301e4
Igor BALLO <balloigor66@gmail.com>
2023-10-13 15:48:00 +0000

    solid architecture

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { PrismaModule } from "../prisma/prisma.module";
+import { PrismaModule } from "src/shared/prisma/prisma.module";
 
 @Module({
   imports: [PrismaModule],
--- src/app/modules/products/products.service.ts
+++ src/app/modules/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import {PrismaService} from "src/prisma/prisma.service";
+import {PrismaService} from "src/shared/prisma/prisma.service";
 
 @Injectable()
 export class ProductsService {
